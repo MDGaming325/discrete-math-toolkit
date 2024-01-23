abdfded34ad0046ecc0d4b7872783c69aabe81f1
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2024-01-23 18:38:40 +0100

    refactor: :recycle:

--- src/pretty.py
+++ src/pretty.py
@@ -1,7 +1,8 @@
 from typing import *
 import recursion as rec
 
-def recursion(recursion:Union[rec.FirstLinealRecursion, rec.SecondLinealRecursion]) -> str:
+
+def recursion(recursion: Union[rec.FirstLinealRecursion, rec.SecondLinealRecursion]) -> str:
 
     if type(recursion) == rec.FirstLinealRecursion:
 
@@ -15,13 +16,13 @@ def recursion(recursion:Union[rec.FirstLinealRecursion, rec.SecondLinealRecursio
             n1_coeff = ''
 
         return f'{n_coeff}a(n) = {n1_coeff}a(n-1)'
-    
+
     else:
 
         n_coeff = recursion.n_coeff
         n1_coeff = recursion.n1_coeff
         n2_coeff = recursion.n2_coeff
-    
+
         if n_coeff == 1:
             n_coeff = ''
 
@@ -32,4 +33,3 @@ def recursion(recursion:Union[rec.FirstLinealRecursion, rec.SecondLinealRecursio
             n2_coeff = ''
 
         return f'{n_coeff}a(n) = {n1_coeff}a(n-1) {n2_coeff}a(n-1)'
-
