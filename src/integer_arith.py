efbe36717e2273f7086dae3f9b3ef30ada53e52e
Mohamed Ahmed <mohamedtaha2005@gmail.com>
2024-01-24 13:51:16 +0100

    feat: :sparkles: Introduces documentation and vital functions
    Added fermat_primality, divp_algorithm, divp_factorization, fermat_factorization, totient and elegant_eaa
    
    Removed lineal_factorization, added commets to functions and formated acording pep8

--- src/integer_arith.py
+++ src/integer_arith.py
@@ -4,10 +4,21 @@ import random
 
 EuclideanDivision = NamedTuple('EuclideanDivision', [(
     'dividend', int), ('divisor', int), ('quotient', int), ('remainder', int)])
-MRForm = NamedTuple('MRForm', [('s', int), ('r', int)])
 
+EuclideanExtended = NamedTuple('EuclideanExtended', [('d',int), ('alpha', int), ('beta', int)])
 
 def sieve_of_eratosthenes(n: int) -> List[int]:
+    """
+    The sieve_of_eratosthenes function takes a number n and returns all prime numbers less than or equal to n.
+        The function uses the Sieve of Eratosthenes algorithm, which is an ancient method for finding prime numbers.
+        It works by first creating a list of all integers from 2 up to the inputted number (n). Then it iterates through 
+        each element in this list and removes any multiples of that element from the list. After removing these multiples, 
+        only primes remain in the list.
+
+    :param n: int: Specify the upper limit of the range of numbers to be checked
+    :return: A list of prime numbers up to n
+
+    """
 
     primes = [2]
     not_primes = list()
@@ -25,6 +36,18 @@ def sieve_of_eratosthenes(n: int) -> List[int]:
 
 
 def modular_power(b: int, e: int, n: int) -> int:
+    """
+    The modular_power function takes in three integers, b, e and n.
+    It returns the result of b^e (mod n).
+
+
+    :param b: int: Represent the base of the modular power
+    :param e: int: Specify the exponent
+    :param n: int: Specify the modulus
+    :return: The result of b^e mod n
+
+    """
+
     res = 1
     exp_bin = bin(e)[2:]
     ins = [exp_bin[0]]
@@ -42,7 +65,19 @@ def modular_power(b: int, e: int, n: int) -> int:
     return res
 
 
-def fermat_primality(n: int, confidence: int) -> bool:
+def fermat_primality(n: int, confidence: int = 100) -> bool:
+    """
+    The fermat_primality function takes in a number n and a confidence level, 
+    and returns True if the number is prime with high probability. The function 
+    uses Fermat's Little Theorem to test for primality. Please note that this
+    is a probability test. 
+
+    :param n: int: Specify the number to test for primality
+    :param confidence: int: Determine how many times the function should run
+    :return: True if n is a PROBABLY prime number and false otherwise.
+
+    """
+
     for _ in range(confidence):
         b = random.randint(2, n-1)
         if modular_power(b, n, n) != b % n:
@@ -50,29 +85,110 @@ def fermat_primality(n: int, confidence: int) -> bool:
     return True
 
 
-def lineal_factorization(n: int) -> List[Tuple[int, int]]:
+def divp_algorithm(n: int) -> int:
+    """
+    The divp_algorithm function takes a positive integer n as input and returns the smallest prime divisor of n.
+        If n is prime, then it returns itself.
 
-    brk = True
-    quotient = None
+        The algorithm works by first checking if 2 or 3 are divisors of the number, since these are the only even primes. 
+        Then it checks all odd numbers up to sqrt(n) for primality using modular arithmetic.
+
+    :param n: int: Specify that the function takes an integer as input
+    :return: The smallest prime divisor of n
+
+    """
+
+    if n > 0:
+        if n % 2 == 0:
+            return 2
+        elif n % 3 == 0:
+            return 3
+
+        m = floor(sqrt(n)-1//6)
+
+        for k in range(1, m+1):
+            if (6*k-1) % n == 0:
+                return 6*k-1
+            elif (6*k+1) % n == 0:
+                return 6*k-1
+            elif k == m:
+                return n
+
+    else:
+        raise ValueError('n must be positive')
+
+
+def divp_factorization(n: int) -> List[Tuple[int, int]]:
+    """
+    The divp_factorization function takes a positive integer n and returns the prime factorization of n.
+
+        Args:
+            n (int): A positive integer.
+
+        Returns: 
+            List[Tuple[int, int]]: The prime factorization of n as a list of tuples where each tuple is in the form (p, e) where p is a prime number and e is its exponent in the factorization.
+
+    :param n: int: Pass the number to be factored into the function
+    :return: A list of tuples
+
+    """
+
     res = DefaultDict(int)
-    floor_sqrt_n = floor(sqrt(n))
-    primes_to_check = sieve_of_eratosthenes(floor_sqrt_n)
+    while n > 1:
+        p = divp_algorithm(n)
+        res[p] += 1
+        n = n//p
 
-    while quotient != 1 and brk:
-        for prime in primes_to_check:
+    return res.items()
 
-            if n % prime == 0:
-                while n % prime == 0 and (quotient == None or prime < sqrt(quotient)):
-                    res[prime] += 1
-                    quotient = n//prime
-                    n = quotient
 
-            elif prime == primes_to_check[-1]:
-                res[quotient] += 1
-                brk = False
-                break
+def fermat_factorization(n: int) -> Tuple[int, int]:
+    """
+    The fermat_factorization function takes an integer n and returns
+    a tuple of two integers, the first being the factorization of n.
+    The second is always 1.
 
-    return list(res.items())
+        Args:
+            n (int): An integer to be factored using Fermat's method.
+
+    :param n: int: Specify the value of n in the equation x^2 - n = y^2
+    :return: A tuple of prime numbers that multiplied equals n
+
+    """
+
+    if n % 2 != 0:
+        x = floor(sqrt(n))+1
+        candidate = x**2 - n
+        while not candidate.is_integer():
+            x: int = x + 1
+            candidate: int = x**2 - n
+        return int(candidate)
+    else:
+        raise ValueError('n must be even')
+
+
+def totient(n: int) -> int:
+    """
+    The totient function, also known as Euler's totient function,
+    is a multiplicative function that counts the number of positive integers
+    less than or equal to n that are relatively prime to n.
+
+    :param n: int: Specify the number that we want to find the totient of
+    :return: The totient of n
+
+    """
+
+    if fermat_primality(n):
+        return n - 1
+
+    else:
+        primes = divp_factorization(n)
+        res = n
+
+        for key, _ in primes:
+            res = res * (1-1/key)
+
+        return int(res)
 
 
 def euclidean_algorithm(a: int, b: int) -> List[EuclideanDivision]:
@@ -103,6 +219,31 @@ def euclidean_algorithm(a: int, b: int) -> List[EuclideanDivision]:
         remainder = r
 
     return operations
+
+
+def elegant_eea(a: int, b: int) -> EuclideanExtended:
+    """
+    The elegant_eea function is an elegant implementation of the extended Euclidean algorithm.
+    It uses the same logic as the eea function, but it does use the invariant method.
+    
+    :param a: int: Any int
+    :param b: int: Any int or the mod to get the inverse of a in Zb
+    :return: An EuclideanExtended object
+    
+    """
+    
+    r, old_r = b, a
+    s, old_s = 0, 1
+    t, old_t = 1, 0
+
+    while r > 0:
+        c = int(floor(old_r/r))
+
+        old_r, r = r, old_r - c*r
+        old_s, s = s, old_s - c*s
+        old_t, t = t, old_t - c*t
+
+    return EuclideanExtended(old_r, old_s, old_t)
 
 
 def fermat_factorization(n: int) -> Tuple[int, int]:
